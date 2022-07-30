747d6dc4fd5da4d5674f4214344afc104fdabd13
DynoTheCrux <76056168+DynoTheCrux@users.noreply.github.com>
2022-07-30 11:14:03 +0200

    Update train.py

--- train/train.py
+++ train/train.py
@@ -141,7 +141,7 @@ def train_net(
   model.fit(train_data,
             epochs=epochs,
             validation_data=valid_data,
-            steps_per_epoch=int((train_len - 1) / batch_size + 1),
+            steps_per_epoch=500,
             validation_steps=int((valid_len - 1) / batch_size + 1),
             callbacks=[tensorboard_callback])
   loss, acc = model.evaluate(test_data)
