bd232ceb46d0f46144c2828157ca5a619d616d74
DynoTheCrux <76056168+DynoTheCrux@users.noreply.github.com>
2022-07-30 16:07:42 +0200

    Update train.py

--- train/train.py
+++ train/train.py
@@ -55,14 +55,14 @@ def build_cnn(seq_length):
           padding="same",
           activation="relu",
           input_shape=(seq_length, 1, 1)),  # output_shape=(batch, 128, 3, 8)
-      tf.keras.layers.MaxPool2D((3, 1)),  # (batch, 42, 1, 8)
-      tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
-      tf.keras.layers.Conv2D(16, (4, 1), padding="same",
-                             activation="relu"),  # (batch, 42, 1, 16)
+      # tf.keras.layers.MaxPool2D((3, 1)),  # (batch, 42, 1, 8)
+      # tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
+      # tf.keras.layers.Conv2D(16, (4, 1), padding="same",
+      #                        activation="relu"),  # (batch, 42, 1, 16)
       tf.keras.layers.MaxPool2D((1, 1), padding="same"),  # (batch, 14, 1, 16)
       tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
       tf.keras.layers.Flatten(),  # (batch, 224)
-      tf.keras.layers.Dense(16, activation="softmax"),  # (batch, 16)
+      tf.keras.layers.Dense(16, activation="relu"),  # (batch, 16)
       tf.keras.layers.Dropout(0.1),  # (batch, 16)
       tf.keras.layers.Dense(4, activation="softmax")  # (batch, 4)
   ])
