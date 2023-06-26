d3d1ecf2977c12ec5211566af6702e89841972a6
DynoTheCrux <76056168+DynoTheCrux@users.noreply.github.com>
2023-06-26 11:00:40 +0200

    Update data_augmentation.py

--- train/data_augmentation.py
+++ train/data_augmentation.py
@@ -60,15 +60,15 @@ def augment_data(original_data, original_label):
       new_data.append(tmp_data)
       new_label.append(label)
     # Time warping
-    fractions = [(3, 2), (5, 3), (2, 3), (3, 4), (9, 5), (6, 5), (4, 5)]
-    for molecule, denominator in fractions:
-      wrapped_data = time_wrapping(molecule, denominator, data)
-      if len(wrapped_data) != 0:
-        new_data.append(wrapped_data)
-        new_label.append(label)
-    # Movement amplification
-    for molecule, denominator in fractions:
-      new_data.append(
-          (np.array(data, dtype=np.float32) * molecule / denominator).tolist())
-      new_label.append(label)
+    # fractions = [(3, 2), (5, 3), (2, 3), (3, 4), (9, 5), (6, 5), (4, 5)]
+    # for molecule, denominator in fractions:
+    #   wrapped_data = time_wrapping(molecule, denominator, data)
+    #   if len(wrapped_data) != 0:
+    #     new_data.append(wrapped_data)
+    #     new_label.append(label)
+    # # Movement amplification
+    # for molecule, denominator in fractions:
+    #   new_data.append(
+    #       (np.array(data, dtype=np.float32) * molecule / denominator).tolist())
+    #   new_label.append(label)
   return new_data, new_label
